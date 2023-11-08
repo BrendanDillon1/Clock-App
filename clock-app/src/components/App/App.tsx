5c48bd6b019ca9c86d5e0c5eb4278a166268d08b
Brendan Dillon <bdillon9@gmail.com>
2023-11-07 19:32:01 -0500

    basics for the app.tsx

--- clock-app/src/components/App/App.tsx
+++ clock-app/src/components/App/App.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import { Clock } from "../../../utils/interfaces";
+import Settings from "../Settings";
+import Clocks from "../Clocks";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [clocks, setClocks] = useState<Clock[]>([]);
+  const updateClock = (clockToUpdate: Clock) => {};
+
+  return (
+    <div className="App">
+      <Settings clocks={clocks} updateClock={updateClock} />
+      <Clocks clocks={clocks} />
+    </div>
+  );
+};
 
 export default App;
